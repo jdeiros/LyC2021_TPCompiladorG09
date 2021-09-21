3f5b82defdb24412ffdca9e81f8527728424137e
Jerónimo Deiros <jeronimodeiros@gmail.com>
2021-09-21 01:01:49 -0300

    primer entrega

---
+++ Grupo09/GeneraExe.bat
@@ -0,0 +1,13 @@
+flex Lexico.l
+pause
+bison -dyv Sintactico.y
+pause
+gcc.exe funciones.c lex.yy.c y.tab.c -o Primera.exe
+pause
+Primera.exe Prueba.txt
+del lex.yy.c
+del y.tab.c
+del y.output
+del y.tab.h
+del Primera.exe
+pause
